7ea50c09631c437ca9fcf4629b99d52829363080
nazmul-devs <nazmulhosenm668@gmail.com>
2024-01-05 15:40:17 +0600

    simple dsa with array

--- array.js
+++ array.js
@@ -5,8 +5,36 @@ class MyArray {
   }
 
   // INSERT ITEM
-  insert(item) {
+  push(item) {
     this.data[this.length] = item;
     this.length++;
   }
+
+  // GET ITEM
+  get(index) {
+    return this.data[index];
+  }
+
+  // REMOVE LAST ITEM
+  pop() {
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+
+  // REMOVE ITEM
+  remove(index) {
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.index - 1];
+    this.length--;
+  }
 }
+
+const array = new MyArray();
+
+array.push(122);
+array.push(244);
+array.push(254);
+array.remove(1);
+console.log(array);
